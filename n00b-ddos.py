ad989dcc54d61ed872a9800e8c4b7b801441c81e
Kafulkaff99 <kafulkaff02@gmail.com>
2024-10-28 14:22:10 +0700

    Update n00b-ddos.py

--- n00b-ddos.py
+++ n00b-ddos.py
@@ -11,8 +11,25 @@ if os.name == 'nt':
     os.system("cls")
 else:
     os.system("clear")
-
-
+print(" ")
+print("                 @@            ")
+print("               @@° @@             ")
+print(" @@@   @@@   @@      @@            ")
+print(" @@@ @°  @  @@        @@           ")
+print(" @@@     @ @@          @@           ")
+print(" @@      @ @@           @@           ")
+print(" @       @ @@           @@           ")
+print(" °       @                        ")
+print(" •       •                          ")
+print("                                    ")
+print("                                    ")
+print("                                     ")
+print("                                     ")
+print("                                     ")
+print("                                     ")
+print("[" + B "" + R + "#" + N + "] " + B "" + R + "Author : n00d" + N + " n00d ddos From - " + B "" + R + "za99" + N)
+print("\033[32m_\033[0m")
+print("\033[32m_\033[0m")
 url = input("URL:  ").strip()
 
 
