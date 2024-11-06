e542deda10303da1a219ae5e4e9035454df34b29
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-06 12:52:35 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ if c == "0":
     os.system("npm install os")
     os.system("npm install sys")
     os.system("npm install threading")
-   elif c == "1":
+elif c == "1":
     os.system("apt-get install nodej-lts")
     os.system("apt-get install npm")
     os.system("npm install os")
     os.system("npm install sys")
     os.system("npm install threading")
-print("Done.")
+    print("Done.")
 
 
 
