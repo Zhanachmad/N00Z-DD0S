c2e293dc06add305b269ce52a9f2747250f3a05a
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-06 12:21:53 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("apt-get install nodej-lts")
+    os.system("apt-get install npm")
+    os.system("npm install os")
+    os.system("npm install sys")
+    os.system("npm install threading")
+   elif c == "1":
+os.system("apt-get install nodej-lts")
+    os.system("apt-get install npm")
+    os.system("npm install os")
+    os.system("npm install sys")
+    os.system("npm install threading")
+print("Done.")
+
+
+
